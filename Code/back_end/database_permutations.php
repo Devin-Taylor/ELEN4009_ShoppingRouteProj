9e821111e2b272d96b52f6fb283ab0f722faec08
Devin-Taylor <dev.t03@gmail.com>
2016-04-06 18:29:14 +0200

    able to get permutations table now for three different items

--- Code/back_end/database_permutations.php
+++ Code/back_end/database_permutations.php
@@ -19,18 +19,35 @@
 	   }
 	}
 
-	$sql = "CREATE VIEW EDGARS_MATCH AS SELECT * FROM EDGARS INNER JOIN LIST USING (ITEM)";
-	
-	$ret = pg_query($db, $sql);	
-	checkError($db, $ret, "View created successfully\n");
+	$array = pg_copy_to($db, "LIST");
 
-	$sql = "CREATE VIEW WOOLWORTHS_MATCH AS SELECT * FROM WOOLWORTHS INNER JOIN LIST USING (ITEM)";
-	
-	$ret = pg_query($db, $sql);	
-	checkError($db, $ret, "View created successfully\n");
+	for($i = 0; $i < 6; $i++){
 
-	$sql = "CREATE VIEW ALL_OPTIONS AS SELECT * FROM EDGARS_MATCH CROSS JOIN WOOLWORTHS_MATCH";
+		$sql =<<<EOF
+   		CREATE TABLE $array[$i]
+   		(SHOP CHAR(300) NOT NULL,
+   		PRICE CHAR(20) NOT NULL);
+EOF;
+		$ret = pg_query($db, $sql);
+		checkError($db, $ret, "created successfully\n");
+		$item = (string)$array[$i];
+		$it = trim($item);
+		$params = array($it);
+		$ret = pg_query_params($db, "INSERT INTO $array[$i] SELECT shop, price FROM EDGARS where ITEM = $1", $params);
+		$ret = pg_query_params($db, "INSERT INTO $array[$i] SELECT shop, price FROM WOOLWORTHS where ITEM = $1", $params);
+		$ret = pg_query_params($db, "INSERT INTO $array[$i] SELECT shop, price FROM MAKRO where ITEM = $1", $params);
+		checkError($db, $ret, "added woolworths successfully\n");
+
+	}
+
+	$sql = "CREATE TABLE MATCHES AS SELECT milk.shop AS m_shop, shirt.shop AS s_shop, tv.shop AS t_shop, coalesce(milk.price::float) + coalesce(shirt.price::float) + coalesce(tv.price::float) AS total_price FROM milk CROSS JOIN shirt CROSS JOIN tv";
+
+	$ret = pg_query($db, $sql);
+
+	// get the locations of the stores
+
 	
-	$ret = pg_query($db, $sql);	
-	checkError($db, $ret, "View created successfully\n");
+
+
+
 ?>
