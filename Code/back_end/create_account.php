e95bf4def577bd4a13925921c33bdad02feb4ddf
Devin-Taylor <dev.t03@gmail.com>
2016-04-07 13:55:56 +0200

    changed accounts database name

--- Code/back_end/create_account.php
+++ Code/back_end/create_account.php
@@ -112,7 +112,7 @@
 </html>
 <?php
 $db = pg_connect("host=localhost port=5432 dbname=srrec user=postgres password=srrec");  
-$query = "INSERT INTO login_details VALUES ('$_POST[fullname]','$_POST[password]',  
+$query = "INSERT INTO accounts VALUES ('$_POST[fullname]','$_POST[password]',  
 '$_POST[email]')";  
 $result = pg_query($query);   
 ?>  
