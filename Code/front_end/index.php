960a5ad5c764b0f356689f2e2d57e8bf9da3e92b
mattmarsden04 <mattmarsden04@gmail.com>
2016-04-07 17:12:19 +0200

    Fixed the index.php page so that it displays the current shopping list items and you can both add and remove items

--- Code/front_end/index.php
+++ Code/front_end/index.php
@@ -85,39 +85,75 @@
 					<br><br>
 					<input type="submit" class="large-fld large-btn" name="genlist" value="Update List">
 
-				</div>			
+					<?php
+						$host 			= "host = localhost";
+						$port 			= "port = 5432";
+						$dbname 		= "dbname = srrec";
+						$credentials 	= "user=postgres password=srrec";
+						
+						$conn = pg_connect("$host $port $dbname $credentials") or die ("Error : Unable to open database\n");
+
+						if(isset($_POST['genlist'])) {
+							$array = explode(',', $_POST['additems']);
+							pg_copy_from($conn, "LIST", $array);
+
+							$array = explode(',', $_POST['removeitems']);
+							
+							for($i=0;$i < sizeof($array); $i++) {
+								$ret = pg_query($conn, "DELETE FROM LIST WHERE ITEM = '$array[$i]'");
+							}
+						}
+
+					?>
+
+				</div>
 
 				<div class="column6">
 
+					<h1> Current List</h1>
+					<div class="large-fld">
+ 					<?php
+						// $host 			= "host = localhost";
+						// $port 			= "port = 5432";
+						// $dbname 		= "dbname = srrec";
+						// $credentials 	= "user=postgres password=srrec";
+						
+						// $conn = pg_connect("$host $port $dbname $credentials") or die ("Error : Unable to open database\n");
+
+						$current_list = pg_copy_to($conn, "LIST");
+
+						echo '<p>'.implode(', ', $current_list).'</p>';
+					?>
+					</div>
+					<br><br>
+
+		</form>
+
+		<form action="" method="POST">
+
 					<h1> Add Location</h1>
 					<input type="text" class="large-fld" name="addlocation" 
-					value="" placeholder="location..."><br><br>
+					value="" placeholder="location...">
+					<br><br>
 
 					<h1> Preferred Optimisation</h1>
 					<select name="optimisation" class="large-fld" required>
 					  <option value="" disabled selected>Select</option>
 					  <option value="opt1">Shortest Total Distance</option>
-					  <option value="opt2">Least Total Costs</option>
+					  <option value="opt2">Lowest Total Costs</option>
 					  <option value="opt3">Shortest Total Time</option>
 					</select>
 					<br><br>
 
-					<h1> Space</h1>
-					<input name="space" class="large-fld">
-					<!-- <br><br><br><br><br><br> -->
-
-					<br><br>
 					<input type="submit" class="large-fld large-btn" name="genroute" value="Generate Route">
 
 					<!-- <input type="checkbox" name="distance" value=""> Least Total Distance<br>
 					<input type="checkbox" name="cost" value=""> Least Total Cost<br>
 					<input type="checkbox" name="time" value=""> Least Total Time<br> -->
 
-
 				</div>
-
 			</div>
-
+		</form>
 	</div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -138,24 +174,3 @@
 
 </body>
 </html>
-
-<?php
-
-	$host 			= "host = localhost";
-	$port 			= "port = 5432";
-	$dbname 		= "dbname = srrec";
-	$credentials 	= "user=postgres password=srrec";
-	
-	$conn = pg_connect("$host $port $dbname $credentials") or die ("Error : Unable to open database\n");
-
-	$array = explode(',', $_POST['additems']);
-	pg_copy_from($conn, "LIST", $array);
-
-	$array = explode(',', $_POST['removeitems']);
-	
-	for($i=0;$i < sizeof($array); $i++) {
-		$ret = pg_query($conn, "DELETE FROM LIST WHERE ITEM = '$array[$i]'");
-
-	}
-
-?>
